bd43bd915d1966b6450438f92ff54a9810afdd53
SalasC2 <christiansalas96@gmail.com>
2016-11-23 17:28:02 -0800

    Refactored code to only use code that specific to apprentice

--- apprentice_teacher.rb
+++ apprentice_teacher.rb
@@ -1,21 +1,14 @@
-class ApprenticeTeacher
-  attr_reader :age, :salary, :phase, :target_raise
-  attr_accessor :name
+require_relative 'teacher'
+
+class ApprenticeTeacher < Teacher
+
+  attr_reader :target_raise
 
   def initialize(options={})
-    @age = options.fetch(:age, 0)
-    @name = options.fetch(:name, "")
+    super
+    # @age = options.fetch(:age, 0)
+    # @name = options.fetch(:name, "")
     @target_raise = 800
-    @phase = 3
-  end
-
-  def offer_high_five
-    "High five!"
-  end
-
-  def set_phase(num)
-    @phase = num
-    "Cool, I've always wanted to teach phase #{num}!"
   end
 
   def teach_stuff
@@ -24,15 +17,6 @@ class ApprenticeTeacher
     response += "*drops crazy knowledge bomb* "
     response += "... You're welcome."
     response
-  end
-
-  def salary=(new_salary)
-    puts "This better be good!"
-    @salary = new_salary
-  end
-
-  def receive_raise(raise)
-    @salary += raise
   end
 
   def set_performance_rating(rating)
@@ -50,4 +34,25 @@ class ApprenticeTeacher
   def attend_training_session
     puts "Whoa. I know ruby-fu"
   end
+
+  # def offer_high_five
+  #   "High five!"
+  # end
+
+  # def set_phase(num)
+  #   @phase = num
+  #   "Cool, I've always wanted to teach phase #{num}!"
+  # end
+  #
+
+  #
+  # def salary=(new_salary)
+  #   puts "This better be good!"
+  #   @salary = new_salary
+  # end
+  #
+  # def receive_raise(raise)
+  #   @salary += raise
+  # end
+  #
 end
